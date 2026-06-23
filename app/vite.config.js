690db3956d5625199c6c45dcd12675948104fa53
Anisa <anisa.duraj@edu.tbz.ch>
2026-06-23 12:33:20 +0200

    app push

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Abschlussprojekt_2026_API-1_durani_F1-App/"
+  base: "/abschlussprojekt_2026_api_1_durani_F1-App/"
 })
