7ad45fc5e01f31b15d290a01b846f54b1d398ddd
Anisa <anisa.duraj@edu.tbz.ch>
2026-06-23 21:44:03 +0200

    PWA hinzugefügt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/Abschlussprojekt_2026_API-1_durani_F1-App/",
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate"
+    })
+  ]
+});
