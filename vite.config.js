d3da63318c9af991f1eb077a811e582f51131aa9
Anisa <anisa.duraj@edu.tbz.ch>
2026-06-24 09:07:59 +0200

    Icon push

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,30 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate"
+      registerType: "autoUpdate",
+      manifest: {
+        name: "F1 App Abschlussprojekt",
+        short_name: "F1 App",
+        description: "Formel 1 App für das Abschlussprojekt 2026",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        // Hier sagen wir dem Handy, wo der Einstiegspunkt ist:
+        scope: "/Abschlussprojekt_2026_API-1_durani_F1-App/",
+        start_url: "/Abschlussprojekt_2026_API-1_durani_F1-App/",
+        icons: [
+          {
+            src: "f1-icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "f1-icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
     })
   ]
 });
